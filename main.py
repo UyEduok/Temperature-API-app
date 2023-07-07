285d77bbda758177acb3de24b60161d1dde7c5bf
UyEduok <uyeduok@gmail.com>
2023-07-07 01:56:25 +0100

    Added functionality that provides a data API of temperature information

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from flask import Flask, render_template
+import pandas as pd
+from collections import OrderedDict
 
 app = Flask(__name__)
 
@@ -10,12 +12,22 @@ def home():
 
 @app.route('/api/v1/<station_id>/<date>/')
 def about(station_id, date):
-    return {
-        station_id: 10,
-        date: 'June',
-        'temperature': 90
-    }
+    # Generate the file path based on the station_id
+    path = 'data/TG_STAID' + str(station_id).zfill(6) + '.txt'
+
+    # Read the CSV file into a DataFrame
+    df = pd.read_csv(path, skiprows=20, parse_dates=['    DATE'])
+
+    # Filter the DataFrame based on the date and retrieve the temperature
+    temp = df.loc[df['    DATE'] == date]['   TG'].squeeze() / 10
+
+    # Return a JSON response with the station_id, date, and temperature
+    return {'StationId': station_id,
+            'Date': date,
+            'Temperature': temp
+            }
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    # Run the Flask app in debug mode on port 5001
+    app.run(debug=True, port=5001)
